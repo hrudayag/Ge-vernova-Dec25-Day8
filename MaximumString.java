23c4e9530fa39764d6cf3bfd1a452d73c16ba598
Hrudaya Guttikonda <hrudaya_g@Hrudayas-MacBook-Air.local>
2025-12-24 13:29:07 +0530

    Added UC3

---
+++ MaximumString.java
@@ -0,0 +1,15 @@
+public class MaximumString {
+    public static String testMaximum(String a, String b, String c) {
+        String max = a;
+        if (b.compareTo(max) > 0)
+            max = b;
+        if (c.compareTo(max) > 0)
+            max = c;
+        return max;
+    }
+    public static void main(String[] args) {
+        System.out.println(testMaximum("Peach", "Apple", "Banana"));
+        System.out.println(testMaximum("Apple", "Peach", "Banana"));
+        System.out.println(testMaximum("Apple", "Banana", "Peach"));
+    }
+}
